41cfdaa1165af8e1c5fdfd38a005f3408250a34d
SheetJS <dev@sheetjs.com>
2017-02-10 11:23:01 -0800

    version bump 0.8.3: BIFF2 read/write
    - basic support for parsing BIFF2-4
    - basic support for writing BIFF2
    - cleaned up some bad substr uses for IE6 compatibility
    - added flow type annotations for xlsx.flow.js
    - added numerous null guards (fixes #255 h/t @martinheidegger)
    - README cleanup (fixes #539 h/t @oliversalzburg)
    - pin jszip to local version (closes #408 h/t @limouri)
    
    bower issues:
    
    |  id  | author            | comment                                   |
    |-----:|:------------------|:------------------------------------------|
    | #254 | @kkirsche         | fixes #254 by removing version from json  |
    | #165 | @vincentcialdella | fixes #165 by changing default script     |
    | #180 | @owencraig        | fixes #180 by using xlsx.core.min.js      |
    
    format issues:
    
    |  id  | author            | comment                                   |
    |-----:|:------------------|:------------------------------------------|
    | #271 | @morstaine        | fixes #271 by reworking related parse fns |
    | #504 | @JanSchuermannPH  | fixes #504 detect FullPaths h/t @Mithgol  |
    | #508 | @basma-emad       | fixes #508 offending file used `x:` NS    |

--- bits/47_styxml.js
+++ bits/47_styxml.js
@@ -1,72 +1,46 @@
 /* 18.8.21 fills CT_Fills */
 function parse_fills(t, opts) {
-  styles.Fills = [];
-  var fill = {};
-  t[0].match(tagregex).forEach(function (x) {
-    var y = parsexmltag(x);
-    switch (y[0]) {
-      case '<fills':
-      case '<fills>':
-      case '</fills>':
-        break;
+	styles.Fills = [];
+	var fill = {};
+	t[0].match(tagregex).forEach(function(x) {
+		var y = parsexmltag(x);
+		switch(y[0]) {
+			case '<fills': case '<fills>': case '</fills>': break;
 
-      /* 18.8.20 fill CT_Fill */
-      case '<fill>':
-        break;
-      case '</fill>':
-        styles.Fills.push(fill);
-        fill = {};
-        break;
+			/* 18.8.20 fill CT_Fill */
+			case '<fill>': break;
+			case '</fill>': styles.Fills.push(fill); fill = {}; break;
 
-      /* 18.8.32 patternFill CT_PatternFill */
-      case '<patternFill':
-        if (y.patternType) fill.patternType = y.patternType;
-        break;
-      case '<patternFill/>':
-      case '</patternFill>':
-        break;
+			/* 18.8.32 patternFill CT_PatternFill */
+			case '<patternFill':
+				if(y.patternType) fill.patternType = y.patternType;
+				break;
+			case '<patternFill/>': case '</patternFill>': break;
 
-      /* 18.8.3 bgColor CT_Color */
-      case '<bgColor':
-        if (!fill.bgColor) fill.bgColor = {};
-        if (y.indexed) fill.bgColor.indexed = parseInt(y.indexed, 10);
-        if (y.theme) fill.bgColor.theme = parseInt(y.theme, 10);
-        if (y.tint) fill.bgColor.tint = parseFloat(y.tint);
+			/* 18.8.3 bgColor CT_Color */
+			case '<bgColor':
+				if(!fill.bgColor) fill.bgColor = {};
+				if(y.indexed) fill.bgColor.indexed = parseInt(y.indexed, 10);
+				if(y.theme) fill.bgColor.theme = parseInt(y.theme, 10);
+				if(y.tint) fill.bgColor.tint = parseFloat(y.tint);
+				/* Excel uses ARGB strings */
+				if(y.rgb) fill.bgColor.rgb = y.rgb.substring(y.rgb.length - 6);
+				break;
+			case '<bgColor/>': case '</bgColor>': break;
 
+			/* 18.8.19 fgColor CT_Color */
+			case '<fgColor':
+				if(!fill.fgColor) fill.fgColor = {};
+				if(y.theme) fill.fgColor.theme = parseInt(y.theme, 10);
+				if(y.tint) fill.fgColor.tint = parseFloat(y.tint);
+				/* Excel uses ARGB strings */
+				if(y.rgb) fill.fgColor.rgb = y.rgb.substring(y.rgb.length - 6);
+				break;
+			case '<fgColor/>': case '</fgColor>': break;
 
-        if (y.theme && themes.themeElements && themes.themeElements.clrScheme) {
-          fill.bgColor.rgb = rgb_tint(themes.themeElements.clrScheme[fill.bgColor.theme].rgb, fill.bgColor.tint || 0);
-          if (opts.WTF) fill.bgColor.raw_rgb = rgb_tint(themes.themeElements.clrScheme[fill.bgColor.theme].rgb,0);
-        }
-        /* Excel uses ARGB strings */
-        if (y.rgb) fill.bgColor.rgb = y.rgb;//.substring(y.rgb.length - 6);
-        break;
-      case '<bgColor/>':
-      case '</bgColor>':
-        break;
-
-      /* 18.8.19 fgColor CT_Color */
-      case '<fgColor':
-        if (!fill.fgColor) fill.fgColor = {};
-        if (y.theme) fill.fgColor.theme = parseInt(y.theme, 10);
-        if (y.tint) fill.fgColor.tint = parseFloat(y.tint);
-
-        if (y.theme && themes.themeElements && themes.themeElements.clrScheme) {
-          fill.fgColor.rgb = rgb_tint(themes.themeElements.clrScheme[fill.fgColor.theme].rgb, fill.fgColor.tint || 0);
-          if (opts.WTF) fill.fgColor.raw_rgb = rgb_tint(themes.themeElements.clrScheme[fill.fgColor.theme].rgb,0);
-        }
-
-        /* Excel uses ARGB strings */
-        if (y.rgb) fill.fgColor.rgb = y.rgb;//.substring(y.rgb.length - 6);
-        break;
-      case '<fgColor/>':
-      case '</fgColor>':
-        break;
-
-      default:
-        if (opts.WTF) throw 'unrecognized ' + y[0] + ' in fills';
-    }
-  });
+			default: if(opts.WTF) throw new Error('unrecognized ' + y[0] + ' in fills');
+		}
+	});
 }
 
 function parse_fonts(t, opts) {
@@ -218,34 +192,25 @@ function parse_borders(t, opts) {
         break;
     }
   });
-
 }
-
 /* 18.8.31 numFmts CT_NumFmts */
 function parse_numFmts(t, opts) {
-  styles.NumberFmt = [];
-  var k = keys(SSF._table);
-  for (var i = 0; i < k.length; ++i) styles.NumberFmt[k[i]] = SSF._table[k[i]];
-  var m = t[0].match(tagregex);
-  for (i = 0; i < m.length; ++i) {
-    var y = parsexmltag(m[i]);
-    switch (y[0]) {
-      case '<numFmts':
-      case '</numFmts>':
-      case '<numFmts/>':
-      case '<numFmts>':
-        break;
-      case '<numFmt':
-      {
-        var f = unescapexml(utf8read(y.formatCode)), j = parseInt(y.numFmtId, 10);
-        styles.NumberFmt[j] = f;
-        if (j > 0) SSF.load(f, j);
-      }
-        break;
-      default:
-        if (opts.WTF) throw 'unrecognized ' + y[0] + ' in numFmts';
-    }
-  }
+	styles.NumberFmt = [];
+	var k/*Array<number>*/ = (keys(SSF._table)/*:any*/);
+	for(var i=0; i < k.length; ++i) styles.NumberFmt[k[i]] = SSF._table[k[i]];
+	var m = t[0].match(tagregex);
+	if(!m) return;
+	for(i=0; i < m.length; ++i) {
+		var y = parsexmltag(m[i]);
+		switch(y[0]) {
+			case '<numFmts': case '</numFmts>': case '<numFmts/>': case '<numFmts>': break;
+			case '<numFmt': {
+				var f=unescapexml(utf8read(y.formatCode)), j=parseInt(y.numFmtId,10);
+				styles.NumberFmt[j] = f; if(j>0) SSF.load(f,j);
+			} break;
+			default: if(opts.WTF) throw new Error('unrecognized ' + y[0] + ' in numFmts');
+		}
+	}
 }
 
 function write_numFmts(NF, opts) {
@@ -320,16 +285,14 @@ function parse_cellXfs(t, opts) {
   });
 }
 
-function write_cellXfs(cellXfs) {
-  var o = [];
-  o[o.length] = (writextag('cellXfs', null));
-  cellXfs.forEach(function (c) {
-    o[o.length] = (writextag('xf', null, c));
-  });
-  o[o.length] = ("</cellXfs>");
-  if (o.length === 2) return "";
-  o[0] = writextag('cellXfs', null, {count: o.length - 2}).replace("/>", ">");
-  return o.join("");
+function write_cellXfs(cellXfs)/*:string*/ {
+	var o = [];
+	o[o.length] = (writextag('cellXfs',null));
+	cellXfs.forEach(function(c) { o[o.length] = (writextag('xf', null, c)); });
+	o[o.length] = ("</cellXfs>");
+	if(o.length === 2) return "";
+	o[0] = writextag('cellXfs',null, {count:o.length-2}).replace("/>",">");
+	return o.join("");
 }
 
 /* 18.8 Styles CT_Stylesheet*/
@@ -339,9 +302,10 @@ var parse_sty_xml = (function make_pstyx() {
   var fillsRegex = /<fills([^>]*)>.*<\/fills>/;
   var bordersRegex = /<borders([^>]*)>.*<\/borders>/;
 
-  return function parse_sty_xml(data, opts) {
-    /* 18.8.39 styleSheet CT_Stylesheet */
-    var t;
+return function parse_sty_xml(data, opts) {
+	if(!data) return styles;
+	/* 18.8.39 styleSheet CT_Stylesheet */
+	var t;
 
     /* numFmts CT_NumFmts ? */
     if ((t = data.match(numFmtRegex))) parse_numFmts(t, opts);
@@ -375,26 +339,18 @@ var STYLES_XML_ROOT = writextag('styleSheet', null, {
 
 RELS.STY = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles";
 
-function write_sty_xml(wb, opts) {
+function write_sty_xml(wb/*:Workbook*/, opts)/*:string*/ {
+	var o = [XML_HEADER, STYLES_XML_ROOT], w;
+	if((w = write_numFmts(wb.SSF)) != null) o[o.length] = w;
+	o[o.length] = ('<fonts count="1"><font><sz val="12"/><color theme="1"/><name val="Calibri"/><family val="2"/><scheme val="minor"/></font></fonts>');
+	o[o.length] = ('<fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill></fills>');
+	o[o.length] = ('<borders count="1"><border><left/><right/><top/><bottom/><diagonal/></border></borders>');
+	o[o.length] = ('<cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>');
+	if((w = write_cellXfs(opts.cellXfs))) o[o.length] = (w);
+	o[o.length] = ('<cellStyles count="1"><cellStyle name="Normal" xfId="0" builtinId="0"/></cellStyles>');
+	o[o.length] = ('<dxfs count="0"/>');
+	o[o.length] = ('<tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleMedium4"/>');
 
-  if (typeof style_builder != 'undefined' && typeof 'require' != 'undefined') {
-    return style_builder.toXml();
-  }
-
-  var o = [XML_HEADER, STYLES_XML_ROOT], w;
-  if ((w = write_numFmts(wb.SSF)) != null) o[o.length] = w;
-  o[o.length] = ('<fonts count="1"><font><sz val="12"/><color theme="1"/><name val="Calibri"/><family val="2"/><scheme val="minor"/></font></fonts>');
-  o[o.length] = ('<fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill></fills>');
-  o[o.length] = ('<borders count="1"><border><left/><right/><top/><bottom/><diagonal/></border></borders>');
-  o[o.length] = ('<cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>');
-  if ((w = write_cellXfs(opts.cellXfs))) o[o.length] = (w);
-  o[o.length] = ('<cellStyles count="1"><cellStyle name="Normal" xfId="0" builtinId="0"/></cellStyles>');
-  o[o.length] = ('<dxfs count="0"/>');
-  o[o.length] = ('<tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleMedium4"/>');
-
-  if (o.length > 2) {
-    o[o.length] = ('</styleSheet>');
-    o[1] = o[1].replace("/>", ">");
-  }
-  return o.join("");
+	if(o.length>2){ o[o.length] = ('</styleSheet>'); o[1]=o[1].replace("/>",">"); }
+	return o.join("");
 }
