39f705bafac532f57628413fbff48940d3287c94
Siguza <siguza@siguza.net>
2017-04-17 03:28:38 +0100

    XLSX write page margins
    changes distilled from Siguza/js-xlsx
    see https://github.com/protobi/js-xlsx/pull/19 for discussion

--- xlsx.js
+++ xlsx.js
@@ -10238,6 +10238,10 @@ function parse_ws_xml_margins(margin) {
 	});
 	return o;
 }
+function write_ws_xml_margins(margin) {
+	default_margins(margin);
+	return writextag('pageMargins', null, margin);
+}
 
 function parse_ws_xml_cols(columns, cols) {
 	var seencol = false;
@@ -10547,7 +10551,7 @@ function write_ws_xml(idx, opts, wb, rels) {
 	delete ws['!links'];
 
 	/* printOptions */
-	/* pageMargins */
+	if (ws['!margins'] != null) o[o.length] =  write_ws_xml_margins(ws['!margins'])
 	/* pageSetup */
 
 	var hfidx = o.length;
