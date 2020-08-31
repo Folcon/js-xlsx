7cfba6ef11d059e257e292b13ddae029382398b8
RidaMaryam <rima@ciklum.com>
2020-08-31 13:14:19 +0500

    #Issue-37-with-script: Make runscript to be executed before 'make'

--- file_script.bat
+++ file_script.bat
@@ -1,14 +1,18 @@
-    echo "hello"
-    
-    #adding bits file names to mini.lst
+
+    git checkout master -- bits/47_styxml.js
+
+    sed -i "s|xlscfb.flow.js|dist/xlscfb.js|g" Makefile
+
     echo bits/93_node.js >> mini.lst
     echo bits/94_xmlbuilder.js >> mini.lst
-    #replace the existing file name where needed
+
     sed -i -e 's/95_api/92_api/g' mini.lst
     echo bits/95_stylebuilder.js >> mini.lst
-    #renaming existng files to maintain the sequence of code being generated in final xlsx file
     cd bits
     mv 95_api.js 92_api.js
     mv 97_node.js 93_node.js
     mv 91_xmlbuilder.js 94_xmlbuilder.js 
     mv 92_stylebuilder.js 95_stylebuilder.js
+
+
+
